936093b05802f9b14169ecb98d3ed91b7b93a389
Jeff Freeman <jeff@incrediblemarketing.com>
2019-02-13 13:28:25 -0800

    Font urls that had spaces fixed.

--- includes/customizer/typography/styles.php
+++ includes/customizer/typography/styles.php
@@ -6,17 +6,19 @@
 
 
 function im_fonts_scripts() {
-    $headings_font_h1 = esc_html(get_theme_mod('im_fonts_h1_fonts'));
-    $headings_font_h2 = esc_html(get_theme_mod('im_fonts_h2_fonts'));
-    $headings_font_h3 = esc_html(get_theme_mod('im_fonts_h3_fonts'));
-    $headings_font_h4 = esc_html(get_theme_mod('im_fonts_h4_fonts'));
-    $headings_font_h5 = esc_html(get_theme_mod('im_fonts_h5_fonts'));
-    $headings_font_h6 = esc_html(get_theme_mod('im_fonts_h6_fonts'));
+    $headings_font_h1 = esc_html(get_theme_mod('im_fonts_h1_family'));
+    $headings_font_h2 = esc_html(get_theme_mod('im_fonts_h2_family'));
+    $headings_font_h3 = esc_html(get_theme_mod('im_fonts_h3_family'));
+    $headings_font_h4 = esc_html(get_theme_mod('im_fonts_h4_family'));
+    $headings_font_h5 = esc_html(get_theme_mod('im_fonts_h5_family'));
+    $headings_font_h6 = esc_html(get_theme_mod('im_fonts_h6_family'));
     $headings_font =  array($headings_font_h1, $headings_font_h2, $headings_font_h3, $headings_font_h4, $headings_font_h5, $headings_font_h6);
     $headings_font = array_unique($headings_font); 
     $headings_font = remove_empty($headings_font);
     $headings_font = implode('|', $headings_font);
-    $body_font = esc_html(get_theme_mod('im_fonts_body_fonts'));
+    $headings_font = str_replace(' ', '+', $headings_font);
+    $body_font = esc_html(get_theme_mod('im_fonts_body_family'));
+    $body_font = str_replace(' ', '+', $body_font);
     
     if( $headings_font ) {
         wp_enqueue_style( 'im_fonts-headings-fonts', add_query_arg( 'family', $headings_font, '//fonts.googleapis.com/css' ) );
