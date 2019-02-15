281776dda07b6ea8f58c0b61e3aa0659567d7a40
Jeff Freeman <jeff@incrediblemarketing.com>
2019-02-15 15:49:49 -0800

    Include gsap
    NPM and enqueued TweenMax.js

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -21,6 +21,7 @@
 					this.scrollIt();
 					this.dataCss();
 					this.stickySiteNav();
+                    this.scrollMagic();
 
 				},
 				mqState : {
@@ -121,7 +122,49 @@
 						}
 					});
 
-				}
+				},
+                scrollMagic: function() {
+                    var blocks = $('.site-wrap').children('.block'),
+                        ctrl = new ScrollMagic.Controller();
+
+                    blocks.each(function(i, item) {
+                        new ScrollMagic.Scene({
+                                triggerElement: item,
+                                duration: item.outerHeight,
+                                triggerHook: 0.9,
+                                reverse: false
+                            })
+                            .on("enter", function() {
+                                var current = blocks.eq(i);
+                                // Example usage
+                                // if (current.hasClass('hero')) {
+                                //     var tl = new TimelineMax({ paused: true, force3D: true, ease: Circ.easeInOut }),
+                                //         bg = current.children('.bg-image'),
+                                //         parallaxer = current.find('.parallax').children(),
+                                //         opts = { delay: 0.6 },
+                                //         duration = 1.5,
+                                //         fromOpts = { drawSVG: '0' },
+                                //         toOpts = { drawSVG: '100% 0', visibility: 'visible', ease: Circ.easeInOut },
+                                //         path = document.querySelectorAll('.hero svg path');
+
+                                //     if (winWidth >= mobile) {
+                                //         var scene = document.getElementById('scene'),
+                                //             parallax = new Parallax(scene);
+                                //     }
+
+                                //     tl.to(bg, 0.6, { autoAlpha: 1 });
+                                //     tl.to(parallaxer, 0.6, { opacity: 1 }, '-=0.3');
+                                //     tl.set(path, { fill: '#343130', attr: { 'fill-opacity': 0 } });
+                                //     tl.fromTo(path, duration, fromOpts, toOpts);
+                                //     tl.to(path, 0.6, { attr: { 'fill-opacity': 1 } });
+                                //     tl.play();
+                                // }
+                            })
+                            // Comment "addIndicators()" in/out to use
+                            // .addIndicators()
+                            .addTo(ctrl);
+                    });
+                }
 
 			},
 		};
